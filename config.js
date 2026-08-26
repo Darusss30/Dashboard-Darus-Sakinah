a4cdb0a717ecd9dd473c53946ad409cb1b98f72a
Akhmad Darus Salam <121310190+Darusss30@users.noreply.github.com>
2026-08-26 19:31:23 +0700

    Update API URLs in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.DARUS_DASHBOARD_CONFIG = {
   apiUrl: "",
-  employeeApiUrl: "https://n8n.srv1566494.hstgr.cloud/webhook/darus-dashboard-employee-master",
-  scoreApiUrl: "https://n8n.srv1566494.hstgr.cloud/webhook/darus-dashboard-final-score-bulanan",
-  attendanceApiUrl: "https://n8n.srv1566494.hstgr.cloud/webhook/darus-dashboard-attendance",
+  employeeApiUrl: "https://n8n.srv1929172.hstgr.cloud/webhook/darus-dashboard-employee-master",
+  scoreApiUrl: "https://n8n.srv1929172.hstgr.cloud/webhook/darus-dashboard-final-score-bulanan",
+  attendanceApiUrl: "https://n8n.srv1929172.hstgr.cloud/webhook/darus-dashboard-attendance",
 };
